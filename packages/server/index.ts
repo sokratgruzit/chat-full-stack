dd2dc1696d605dadc315398519307f1fee903cbb
sokratgruzit <tavadzed@gmail.com>
2025-12-26 19:23:19 +0300

    handeled openai api key

---
+++ packages/server/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import type { Request, Response } from 'express';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(process.env.OPENAI_API_KEY);
+});
+
+app.listen(port, () => {
+    console.log(`App is running on http://localhost:${port}`);
+});
