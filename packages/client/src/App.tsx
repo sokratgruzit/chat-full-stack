5fc25d440f81635b98f721c4bbeae905a900250d
sokratgruzit <tavadzed@gmail.com>
2025-12-27 20:15:33 +0300

    refactored frontend to modular style

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,8 @@
-import ChatBot from './components/ChatBot';
+import ChatBot from './components/chat/ChatBot';
 
 function App() {
   return (
-    <div className="p-4">
+    <div className="p-4 h-screen w-full">
       <ChatBot />
     </div>
   );
