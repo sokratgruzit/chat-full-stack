159438087c7e84a76fa6ce993ada4e6ac467d48d
sokratgruzit <tavadzed@gmail.com>
2025-12-28 03:11:30 +0300

    improved prompts for better response

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,7 +2,7 @@ import ChatBot from './components/chat/ChatBot';
 
 function App() {
   return (
-    <div className="p-4 h-screen w-full">
+    <div className="p-4 h-screen w-full max-w-xl m-auto">
       <ChatBot />
     </div>
   );
--- packages/server/services/chat.service.ts
+++ packages/server/services/chat.service.ts
@@ -1,10 +1,20 @@
+import fs from 'fs';
+import path from 'path';
 import OpenAI from 'openai';
 import { conversationRepository } from '../repositories/conversation.repository';
+import template from '../prompts/chatbot.txt';
 
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
   baseURL: process.env.OPENAI_BASE_URL,
 });
+
+const parkInfo = fs.readFileSync(
+  path.join(__dirname, '..', 'prompts', 'WonderWorld.md'),
+  'utf-8'
+);
+
+const instructions = template.replace('{{parkInfo}}', parkInfo);
 
 type ChatResponse = {
   id: string;
@@ -19,6 +29,7 @@ export const chatService = {
     const response = await client.responses.create({
       model: 'gpt-4o-mini',
       input: prompt,
+      instructions,
       temperature: 0.2,
       max_output_tokens: 200,
       previous_response_id:
